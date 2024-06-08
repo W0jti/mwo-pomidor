aeeff8f8ce00d43b665d3aff6231d891162a63a7
Wojtek Wiechec <wojciech.wiechec@solutions4delivery.com>
2024-06-08 13:58:22 +0200

    Modified Main class with Apache Common CLI

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,17 @@
 package org.example;
+
 import org.apache.commons.cli.*;
+import org.example.Data.ExcelReaderFacade;
+import org.example.Data.FileSearcher;
+import org.example.model.Task;
+
+import java.io.IOException;
+import java.util.List;
 
 public class Main {
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
         ////// ustaw ścieżkę do swojego pliku z danymi /////
 
         String filePath = "D:\\reporter-dane\\2012\\01\\Kowalski_Jan.xls";
@@ -18,23 +25,23 @@ public class Main {
 
         FileSearcher fileSearcher = new FileSearcher();
         List<String> filePaths = fileSearcher.searchXlsFile(folderPath);
-        for (String filepath: filePaths) {
+        for (String filepath : filePaths) {
             System.out.println("Reading file: " + filepath);
             List<Task> employeeTasks = excelReader.readEmployeeTasks(filepath);
-            for (Task task: employeeTasks) {
+            for (Task task : employeeTasks) {
                 System.out.println(task);
             }
             System.out.println("\n\n");
         }
-    public static void main(String[] args) throws ParseException {
+
         CommandLineParser parser = new DefaultParser();
         Options options = new Options();
         CommandLine cmd = parser.parse(options, args);
 
-        options.addOption("source", true,"Source of the files");//-source resources
-        options.addOption("reptype", true,"Type of the report");//-reptype 1
-        options.addOption("datefilter", true,"Filter by dates");//-datefilter 2024/04/10-2024/05/20
-        options.addOption("employeefilter",true, "Filter by employee");//-employeefilter Kowalski_Jan
+        options.addOption("source", true, "Source of the files");//-source resources
+        options.addOption("reptype", true, "Type of the report");//-reptype 1
+        options.addOption("datefilter", true, "Filter by dates");//-datefilter 2024/04/10-2024/05/20
+        options.addOption("employeefilter", true, "Filter by employee");//-employeefilter Kowalski_Jan
         options.addOption("taskfilter", true, "Filter bys task");//-taskfilter spotkanie
         options.addOption("help", false, "Help");//-help
 
@@ -46,15 +53,45 @@ public class Main {
         String taskFilter = cmd.getOptionValue("taskfilter");
         String help = cmd.getOptionValue("help");
 
-        if (cmd.hasOption("source")){
+        if (cmd.hasOption("source")) {
             //pull data from that source
-        }
-        else {
+            System.out.println("Report is using source files");
+        } else {
             //throw an error
         }
 
-        if(cmd.hasOption("repType")){
+        if (cmd.hasOption("repType")) {
+            //select particular report
+            System.out.println("Report has selected type");
+        } else {
+            //throw an error
+        }
 
+        if (cmd.hasOption("datafilter")){
+            //show data for selected dates
+            System.out.println("Report is datefiltered");
+        } else {
+            //throw an error
+        }
+
+        if (cmd.hasOption("employeefilter")){
+            //show data for selected employee
+            System.out.println("Report is employee filtered");
+        } else {
+            //throw an error
+        }
+
+        if (cmd.hasOption("taskfilter")){
+            //show data for that task
+            System.out.println("Report is taskfiltered");
+        } else {
+            //throw an error
+        }
+
+        if (cmd.hasOption("help")){
+            //show helper
+            System.out.println("Showing help");
         }
     }
+
 }
