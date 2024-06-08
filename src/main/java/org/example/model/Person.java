f7082b4009183c5393823d4737b39ab3d4dd0bbf
boooiler <sebastianowczarek8@gmail.com>
2024-06-08 10:58:10 +0200

    add model data

---
+++ src/main/java/org/example/model/Person.java
@@ -0,0 +1,54 @@
+package org.example.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+public class Person {
+    private final String name;
+    private final List<Task> tasks;
+
+    public Person(String name) {
+        this.name = name;
+        this.tasks = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void addTask(Task task) {
+        this.tasks.add(task);
+    }
+
+    public void addTasks(Collection<Task> tasks) {
+        this.tasks.addAll(tasks);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return name.equals(person.name) &&
+                tasks.equals(person.tasks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, tasks);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", tasks=" + tasks +
+                '}';
+    }
+}
